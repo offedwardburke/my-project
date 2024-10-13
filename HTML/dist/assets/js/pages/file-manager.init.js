411fdcdbfd1a7a698dc19293b98e6d6d910adedf
offedwardburke <edward.burke@southcentraldata.com>
2024-10-13 11:45:49 -0500

    commit

---
+++ HTML/dist/assets/js/pages/file-manager.init.js
@@ -0,0 +1,1 @@
+Dropzone.autoDiscover=!1;var myDropzone=new Dropzone("#myDropzone",{paramName:"file",maxFiles:10,maxFilesize:10,addRemoveLinks:!1,previewsContainer:".dropzone-previews",previewTemplate:document.querySelector("#uploadPreviewTemplate").innerHTML,getPreviewIcon:!0});
